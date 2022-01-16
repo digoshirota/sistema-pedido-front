2bedd9d35a6ca6a08a8d03fea4ef82359a2b6928
digoshirota <digoshirota@gmail>
2022-01-15 23:34:51 -0300

    aaa

--- services/settings.tsx
+++ services/settings.tsx
@@ -7,7 +7,8 @@ export const isTest = () => process.env.isTesting === 'true';
 let ApiBack: string = "";
 
 if (isProd) {
-    ApiBack = 'http://localhost:3333';
+    ApiBack = 'http://15.229.9.243:3333';
+    // ApiBack = 'http://localhost:3333';
 }
 else {
     //ApiBack = 'https://r3investimentos.com.br';
